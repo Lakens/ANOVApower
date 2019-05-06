09139997053266af49eca98eac2af164e5796f30
Daniel Lakens <Lakens@gmail.com>
2019-05-06 21:58:52 +0200

    update mess

--- mess/try_out.R
+++ mess/try_out.R
@@ -17,10 +17,10 @@ sd = 2
 r = 0.8
 labelnames = c("condition", "cheerful", "sad", "voice", "human", "robot")
 
-design_result_new <- ANOVA_design(string = "2b*2w", n = 40, mu = c(0, 0, 0, 0), sd = 2, r = 0.8, labelnames = c("condition", "cheerful", "sad", "voice", "human", "robot"))
-design_result_new$cor_mat
+design_result <- ANOVA_design(string = "2w*2w", n = 40, mu = c(1, 0, 0, 0.5), sd = 2, r = 0.8, labelnames = c("condition", "cheerful", "sad", "voice", "human", "robot"))
+design_result$cor_mat
 
-power_result <- ANOVA_power(design_result_new, alpha_level = 0.05,
+power_result <- ANOVA_power(design_result, alpha_level = 0.05,
                             p_adjust = "none", nsims = 1000)
 
 power_result$plot1
@@ -55,3 +55,53 @@ for(j in 1:factors){
 }
 xxx$cond <- as.character(interaction(xxx[, 1:factors], sep = "_")) #create a new condition variable combine 2 columns (interaction is a cool function!)
 
+library(ANOVApower)
+#test twoway within function
+design_result <- ANOVA_design(string = "2w*2w", n = 20, mu = c(1, 0, 0, 1), sd = 2, r = 0.0)
+power_res <- power_2x2_within_2(design_result)
+power_res <- power_2x2_within(design_result)
+power_res$mean_mat
+power_res$power_A
+power_res$power_B
+power_res$power_AB
+power_result <- ANOVA_power(design_result, alpha_level = 0.05,
+                            p_adjust = "none", nsims = 1000)
+
+
+
+
+
+
+
+mu = c(2,1,4,2)
+n <- 20
+sd <- 5
+r <- 0.8
+
+string = "2w*2w"
+labelnames = c("A", "a1", "a2", "B", "b1", "b2")
+design_result <- ANOVA_design(string = string,
+                              n = n,
+                              mu = mu,
+                              sd = sd,
+                              r = r,
+                              labelnames = labelnames)
+simulation_result <- ANOVA_power(design_result, nsims = 1000)
+
+power_res <- power_2x2_within(design_result = design_result)
+power_res$power_A
+power_res$power_B
+power_res$power_AB
+
+power_res <- power_twoway_within(design_result = design_result)
+power_res$power_A
+power_res$power_B
+power_res$power_AB
+
+design_result <- ANOVA_design(string = "2w*2w", n = 20, mu = c(4, 2, 1, 2), sd = 2, r = 0.6^2, labelnames = labelnames)
+power_res <- power_2x2_within(design_result)
+power_res$mean_mat
+power_res$power_A
+power_res$power_B
+power_res$power_AB
+power_result <- ANOVA_power(design_result, alpha_level = 0.05, p_adjust = "none", nsims = 30000)
