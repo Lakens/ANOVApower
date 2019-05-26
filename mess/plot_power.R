0db85d5d0ae277635719c0396af505f148f46c0f
Daniel Lakens <Lakens@gmail.com>
2019-05-26 21:58:18 +0200

    remove unused tests

--- mess/plot_power.R
+++ mess/plot_power.R
@@ -1,3 +1,6 @@
+design_result <- ANOVA_design(design = "2w*2w", n = 40, mu = c(1, 0, 1, 0),
+                              sd = 2, r = 0.8)
+max_n <- 50
 #' Convenience function to plot power across a range of sample sizes.
 #' @param design_result Output from the ANOVA_design function
 #' @param max_n Maximum sample size in power curve.
@@ -21,33 +24,32 @@ plot_power <- function(design_result, max_n){
   mu = design_result$mu
   sd <- design_result$sd
   r <- design_result$r
-  labelnames = c(design_result$factornames[[1]], design_result$labelnames[[1]])
 
-  n_vec <- seq(from = , to = max_n)
-
-  power_A <- numeric(length(n_vec))
-
-  for (i in 1:length(n_vec)){
+  #Do one ANOVA to get number of power columns
+  length_power <- length(ANOVA_exact(design_result, verbose = FALSE)$main_results$power)
+  power_df <- as.data.frame(matrix(0, ncol = length_power+1, nrow = max_n-7))
+  power_df[,1] <- c(8:max_n)
+  for (i in 1:(max_n-7)){
     design_result <- ANOVA_design(design = design,
-                                  n = n_vec[i],
+                                  n = i+7,
                                   mu = mu,
                                   sd = sd,
                                   r = r,
-                                  labelnames = labelnames)
+                                  plot = FALSE)
 
-    power_res <- ANOVA_exact(design_result)
-
-    power_A[i] <- power_res$power*100
+    power_df[i,2:(1+length_power)] <- ANOVA_exact(design_result, verbose = FALSE)$main_results$power
   }
+j=2
+  for (j in 1:length_power){
 
-  res_df <- data.frame(n_vec, power_A)
-
-  p1 <- ggplot(data=res_df, aes(x = n_vec, y = power_A)) +
-    geom_line( size=1.5) +
-    scale_x_continuous(limits = c(0, max(n_vec))) +
-    scale_y_continuous(limits = c(0, 100)) +
-    theme_bw() +
-    labs(x="Sample size", y = "Power Factor A")
+    p1 <- ggplot(data=power_df, aes(x = power_df[,1], y = power_df[,(1+j)])) +
+      geom_line( size=1.5) +
+      scale_x_continuous(limits = c(0, max(max_n))) +
+      scale_y_continuous(limits = c(0, 100)) +
+      theme_bw() +
+      labs(x="Sample size", y = "Power")
+    print(p1)
+  }
 
   invisible(list(p1 = p1,
                  power_df = data.frame(paste("f = ",
